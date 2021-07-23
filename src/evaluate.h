73feac1936145d2497c499b3e46925ca70ec299f
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-07-23 16:15:37 +0200

    Update to "New default net nn-3b20abec10c1.nnue" https://github.com/official-stockfish/Stockfish/commit/14b673d90f7f486becda4622eb1e8f4b63bb0785

--- src/evaluate.h
+++ src/evaluate.h
@@ -3,7 +3,7 @@
 
 #include "types.h"
 
-#define DefaultEvalFile "nn-33c9d39e5eb6.nnue"
+#define DefaultEvalFile "nn-3b20abec10c1.nnue"
 
 #ifdef NNUE
 enum { EVAL_HYBRID, EVAL_PURE, EVAL_CLASSICAL };
