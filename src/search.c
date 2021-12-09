9bfbad11d30f2355d3c6d25dff4cfd583664a588
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2021-12-09 14:41:17 +0100

    Update to "Double extend more often via LMR"
    https://github.com/official-stockfish/Stockfish/commit/8a8640a761f266979c7130a49ebbc9ed4c680102

--- src/search.c
+++ src/search.c
@@ -1344,7 +1344,7 @@ moves_loop: // When in check search starts from here
       // this may lead to hidden double extensions if newDepth got it own extension
       // before).
       int deeper =   r >= -1                   ? 0
-                   : moveCount <= 3 && r <= -3 ? 2
+                   : moveCount <= 3            ? 2
                    : moveCount <= 5            ? 1
                    : (PvNode && depth > 6)     ? 1
                    :                             0;
